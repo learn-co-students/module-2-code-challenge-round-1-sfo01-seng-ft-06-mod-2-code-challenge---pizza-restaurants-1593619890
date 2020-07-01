ad45ca0f530249aca7f81371edb38e6fef45ac1e
keeks5456 <Akikogreen08@gmail.com>
2020-07-01 11:07:26 -0700

    round 2 CC

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,9 @@
 class Pizza < ApplicationRecord
+  has_many :restaurant_pizzas
+  has_many :restaurants, through: :restaurant_pizzas
+
+  validates :name, presence: true
+  validates :ingredients, presence: true
+  validates :name, uniqueness: { scope: :name, message: "name already exists" }
+
 end
