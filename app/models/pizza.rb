13e2fde8f9990093ff958c4a53c9b1a3c335468d
Guosa_Edebiri <gedebiri@gmail.com>
2020-07-01 11:34:19 -0700

    committed

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,4 @@
 class Pizza < ApplicationRecord
+    has_many :restaurants_pizzas
+    has_many :restaurants, through: :restaurants_pizzas
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get '/restaurants', to: 'restaurants#index'
-  get '/pizzas', to: 'pizzas#index'
+  resources :pizzas, only: [:index, :new, :create, :show]
 end
